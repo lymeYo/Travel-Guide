f71b5a7863211644e8b7371617b5c3fac52773c6
lymeYo <89268803+lymeYo@users.noreply.github.com>
2022-03-18 22:42:24 +0300

    Update DrawerMenu.tsx

--- src/Header/components/DrawerMenu.tsx
+++ src/Header/components/DrawerMenu.tsx
@@ -1,51 +1,51 @@
-import React, { MouseEvent  } from 'react'
-import { Typography, List, ListItem, ListItemText, ListItemIcon, Drawer } from '@mui/material'
-import LocalHotelIcon from '@mui/icons-material/LocalHotel';
-import SettingsIcon from '@mui/icons-material/Settings';
-import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
-import HomeIcon from '@mui/icons-material/Home';
-import { NavLink } from 'react-router-dom';
+//import React, { MouseEvent  } from 'react'
+//import { Typography, List, ListItem, ListItemText, ListItemIcon, Drawer } from '@mui/material'
+//import LocalHotelIcon from '@mui/icons-material/LocalHotel';
+//import SettingsIcon from '@mui/icons-material/Settings';
+//import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+//import HomeIcon from '@mui/icons-material/Home';
+//import { NavLink } from 'react-router-dom';
 
-function MenuList(props: any) {
+//function MenuList(props: any) {
 
-   return (
-      <List>
-            <NavLink to="/">
-               <ListItem button >
-                     <ListItemIcon> <HomeIcon /> </ListItemIcon>
-                     <ListItemText>Главная</ListItemText>
-               </ListItem>
-            </NavLink>
+  // return (
+    //  <List>
+      //      <NavLink to="/">
+        //       <ListItem button >
+          //           <ListItemIcon> <HomeIcon /> </ListItemIcon>
+            //         <ListItemText>Главная</ListItemText>
+              // </ListItem>
+           // </NavLink>
 
-         <ListItem button >
-            <ListItemIcon> <AccountBalanceIcon /> </ListItemIcon>
-            <ListItemText>Достопримечательности</ListItemText>
-         </ListItem>
+        // <ListItem button >
+          //  <ListItemIcon> <AccountBalanceIcon /> </ListItemIcon>
+            //<ListItemText>Достопримечательности</ListItemText>
+      //   </ListItem>
 
-         <ListItem button >
-            <ListItemIcon> <LocalHotelIcon /> </ListItemIcon>
-            <ListItemText>Отели</ListItemText>
-         </ListItem>
+        // <ListItem button >
+          //  <ListItemIcon> <LocalHotelIcon /> </ListItemIcon>
+            //<ListItemText>Отели</ListItemText>
+       //  </ListItem>
 
-         <ListItem button >
-            <ListItemIcon> <SettingsIcon /> </ListItemIcon>
-            <ListItemText>Настройки профиля</ListItemText>
-         </ListItem>
-      </List>
-   )
-}
+         //<ListItem button >
+           // <ListItemIcon> <SettingsIcon /> </ListItemIcon>
+           // <ListItemText>Настройки профиля</ListItemText>
+       //  </ListItem>
+     // </List>
+ //  )
+//}
 
-function DrawerMenu(props: any) {
+//function DrawerMenu(props: any) {
       
-   return (
-      <Drawer
-         anchor={'left'}
-         open={props.isDrawerOpen}
-         onClose={props.toggleDrawer} 
-      >
-         <MenuList />
-      </Drawer>
-   )
-}
+  // return (
+    //  <Drawer
+      //   anchor={'left'}
+        // open={props.isDrawerOpen}
+        // onClose={props.toggleDrawer} 
+    //  >
+      //   <MenuList />
+  //    </Drawer>
+ //  )
+//}
 
-export default DrawerMenu
+//export default DrawerMenu
